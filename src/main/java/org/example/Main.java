5ee7fa0bad2214f38926b165ba769a2e9c0c244d
zxvfc <velya.96@gmail.com>
2024-12-22 14:01:09 +0100

    final implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
@@ -13,43 +16,70 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final String ADDRESS_A = "Санкт-Петербург, Парк Авиаторов";
-    private static final String COORDINATES_A = "30.3008211724963,59.8673116";
-
-    private static final String ADDRESS_B = "Санкт-Петербург, Парк Победы";
-    private static final String COORDINATES_B = "30.3214922,59.8676183";
-
-    private static final String ORS_API_KEY = "здесь-должен-быть-ваш-ключ";
+    private static final String ORS_API_KEY = "апи-ключ-сюда";
     private static final String ORS_URL = "https://api.openrouteservice.org/v2/directions/driving-car";
     private static final String OSM_SEARCH_URL = "https://nominatim.openstreetmap.org/search";
-    private static final String OSM_REVERSE_URL = "https://nominatim.openstreetmap.org/reverse";
+//    private static final String OSM_REVERSE_URL = "https://nominatim.openstreetmap.org/reverse";
+
+    private static final Gson GSON = new Gson();
 
     public static void main(String[] args) throws URISyntaxException, IOException {
-        final String response = doGetRequest(ORS_URL, Map.of(
-                                                     "api_key", ORS_API_KEY,
-                                                     "start", COORDINATES_A,
-                                                     "end", COORDINATES_B
-                                             )
-        );
-        System.out.println(response);
 
-        final String coordinatesAResponse = doGetRequest(OSM_SEARCH_URL, Map.of(
-                                                                 "q", ADDRESS_A,
-                                                                 "format", "json"
-                                                         )
-        );
-        System.out.println(coordinatesAResponse);
+        final Scanner scanner = new Scanner(System.in);
 
-        final String coordinatesBResponse = doGetRequest(OSM_SEARCH_URL, Map.of(
-                                                                 "q", ADDRESS_B,
-                                                                 "format", "json"
-                                                         )
-        );
-        System.out.println(coordinatesBResponse);
+        System.out.print("Введите начальный адрес: ");
+        final String addressA = scanner.nextLine();
+        final JsonObject locationAInfo = doGetRequest(
+                OSM_SEARCH_URL,
+                Map.of("q", addressA, "format", "json")
+        ).getAsJsonArray().get(0).getAsJsonObject();
+
+        final String coordinatesA = locationAInfo.getAsJsonPrimitive("lon").getAsString()
+                + ","
+                + locationAInfo.getAsJsonPrimitive("lat").getAsString();
+
+        System.out.print("Введите конечный адрес: ");
+        final String addressB = scanner.nextLine();
+        final JsonObject locationBInfo = doGetRequest(
+                OSM_SEARCH_URL,
+                Map.of("q", addressB, "format", "json")
+        ).getAsJsonArray().get(0).getAsJsonObject();
+
+        final String coordinatesB = locationBInfo.getAsJsonPrimitive("lon").getAsString()
+                + ","
+                + locationBInfo.getAsJsonPrimitive("lat").getAsString();
+
+        final JsonObject routSegment = doGetRequest(
+                ORS_URL,
+                Map.of(
+                        "api_key", ORS_API_KEY,
+                        "start", coordinatesA,
+                        "end", coordinatesB
+                )
+        ).getAsJsonObject()
+                .getAsJsonArray("features")
+                .get(0)
+                .getAsJsonObject()
+                .getAsJsonObject("properties")
+                .getAsJsonArray("segments")
+                .get(0)
+                .getAsJsonObject();
+
+        System.out.println("Расстояние: " + routSegment.getAsJsonPrimitive("distance") + " м.");
+        System.out.println("Продолжительность маршрута: " + routSegment.getAsJsonPrimitive("duration") + " сек.");
+
+        for (final JsonElement step : routSegment.getAsJsonArray("steps")) {
+            System.out.println("-------------------------------");
+            System.out.println("Дистанция: " + step.getAsJsonObject().getAsJsonPrimitive("distance") + " м.");
+            System.out.println("Продолжительность: " + step.getAsJsonObject().getAsJsonPrimitive("duration") + " сек.");
+            System.out.println("Инструкция: " + step.getAsJsonObject().getAsJsonPrimitive("instruction").getAsString());
+        }
     }
 
-    static String doGetRequest(final String url,
-                               final Map<String, String> queryParams) throws URISyntaxException, IOException {
+    static JsonElement doGetRequest(
+            final String url,
+            final Map<String, String> queryParams
+    ) throws URISyntaxException, IOException {
         final String queryString = buildQueryParams(queryParams);
         final HttpURLConnection connection = (HttpURLConnection) new URI(url + queryString)
                 .toURL()
@@ -64,7 +94,7 @@ public class Main {
             response.append(scanner.nextLine());
         }
 
-        return response.toString();
+        return GSON.fromJson(response.toString(), JsonElement.class);
     }
 
     static String buildQueryParams(final Map<String, String> queryParams) {
@@ -82,8 +112,7 @@ public class Main {
         final StringBuilder stringBuilder = new StringBuilder("?");
         for (int i = 0; i < formattedParams.size(); i++) {
             final String param = formattedParams.get(i);
-            stringBuilder.append(i != 0 ? "&" : "")
-                    .append(param);
+            stringBuilder.append(i != 0 ? "&" : "").append(param);
         }
         return stringBuilder.toString();
     }
